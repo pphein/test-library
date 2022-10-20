2096739e60385a106d21f5f90d34ef86bb3a8fa1
pph <pph>
2022-10-20 14:53:26 +0630

    Added database

--- db/connect.php
+++ db/connect.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$db = new mysqli('localhost', 'root', '', 'library');
+$db = new mysqli('ec2-44-207-126-176.compute-1.amazonaws.com', 'smukvhrxaqmwmj', 'c28ec57244fcf853dd8fa32c9f52406e0471dceae9fc33832e6659ca95496810', 'dfeeq94jnrlerp');
 
 if($db->connect_error){
 	die("Connection failed: ");
